f5c8802c1398413e4038cd5692d8322e907acb30
IdeamosWeb <contacto@ideamosweb.com>
2013-03-19 12:10:40 -0500

    Slide banner

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	nextSlider("");
+	nextSlider();
 	var interval_slide = setInterval(nextSlider, 5000);
 
 	$('.nav a').click(function(){
@@ -8,44 +8,46 @@ $(document).ready(function(){
 		clearInterval(interval_slide);
 	});
 
-	var i = 1;
+});
+
+var i = 1;
+
+function nextSlider(id_sd) {		
+	var count = (id_sd != undefined) ? id_sd : i;			
+ 	
+ 	var all_slides = $("article");
+ 	all_slides.css('left', 0); 
+	var slider = $("article[data-id='" + count + "']");
+	var prev_slide = $("article[data-id='" + (count - 1) + "']");
+	var next_slide = $("article[data-id='" + (count + 1) + "']"); 
  
-	function nextSlider(id_sd) {		
-		var count = (id_sd != undefined) ? id_sd : i;			
-	 	
-	 	var all_slides = $("article");
-	 	all_slides.css('left', 0); 
-		var slider = $("article[data-id='" + count + "']");
-		var prev_slide = $("article[data-id='" + (count - 1) + "']");
-		var next_slide = $("article[data-id='" + (count + 1) + "']"); 
-	 
-		if(slider.html() == undefined || slider.html() == null) {
-			i = 1;		 
-			nextSlider("");
-		}else{
-			i++;
-			if(count == 1){
-				$('article[data-id]').hide();
-				prev_slide.show();
-				slider.css('left', 600);
-				slider.show();
-				slider.animate({
-					left: parseInt(slider.css('left'), 10) != 600 ? -slider.outerWidth() : 0 
-				});
-			}else{			
-				prev_slide.animate({
-					left: parseInt(slider.css('left'), 10) == 0 ? -slider.outerWidth() : 0 
-				});
+	if(slider.html() == undefined || slider.html() == null) {
+		i = 1;		 
+		nextSlider();
+	}else{
+		i++;
+		if(count == 1){
+			$('article[data-id]').hide();
+			prev_slide.show();
+			slider.css('left', 980);
+			slider.show();
+			slider.animate({
+				left: parseInt(slider.css('left'), 10) != 980 ? -slider.outerWidth() : 0 
+			});
+		}else{			
+			prev_slide.animate({
+				left: parseInt(slider.css('left'), 10) == 0 ? -slider.outerWidth() : 0 
+			});
 
-				$('article[data-id]').hide();
-				prev_slide.show();
+			$('article[data-id]').hide();
+			prev_slide.show();
 
-				slider.css('left', 600);			
-				slider.show();
-				slider.animate({
-					left: parseInt(slider.css('left'), 10) != 600 ? -slider.outerWidth() : 0 
-				});						
-			}
+			slider.css('left', 980);			
+			slider.show();
+			slider.animate({
+				left: parseInt(slider.css('left'), 10) != 980 ? -slider.outerWidth() : 0 
+			});						
 		}
 	}
-});
+}
+
